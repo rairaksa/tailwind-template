9caf4aad705221d86faa8f31f35c3f6ea026c27f
Rai Raksa <rai.r@halosis.co.id>
2021-04-22 17:33:33 +0700

    first commit

---
+++ one-page-profile/tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: {
+  	enabled: false,
+  	content: [
+  		'index.html'
+  	]
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      'sans': ['Poppins']
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
