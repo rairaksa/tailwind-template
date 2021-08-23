68d8c3c1d7b331997e2bc45df7dad414e4c4dd8e
Rai Raksa M <rairaksa7@gmail.com>
2021-08-23 22:58:13 +0700

    init ecommerce structure

---
+++ jongko/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [
+    './index.html'
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      'body': ['Spartan']
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
